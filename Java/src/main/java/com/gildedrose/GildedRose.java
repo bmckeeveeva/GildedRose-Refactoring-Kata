769aec16e8425893068a2f706e350be35bec298e
Bob McKee <bob.mckee@veeva.com>
2025-05-16 11:44:05 -0400

    removed commented out code.  moved quality bounds 0/50 to the main function, so allow item handling functions to be simpler

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,69 +21,22 @@ class GildedRose {
                     break;
                 default:
                     normal(item);
-                    //old(item);
+            }
+
+            if (item.quality < 0) {
+                item.quality = 0;
+            }
+
+            if (item.quality > 50) {
+                item.quality = 50;
             }
         }
     }
 
-    /*
-    private void old(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+    void agedBrie(Item item) {
+        item.quality++;
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
-    }*/
-
-    void agedBrie(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
-        }
-
-        if (item.quality < 50 && item.sellIn < 0) {
             item.quality++;
         }
 
@@ -94,15 +47,13 @@ class GildedRose {
         if (item.sellIn < 1) {
             item.quality = 0;
         } else {
-            if (item.quality < 50) {
+            item.quality++;
+
+            if (item.sellIn < 11) {
                 item.quality++;
             }
 
-            if (item.quality < 50 && item.sellIn < 11) {
-                item.quality++;
-            }
-
-            if (item.quality < 50 && item.sellIn < 6) {
+            if (item.sellIn < 6) {
                 item.quality++;
             }
         }
@@ -115,11 +66,9 @@ class GildedRose {
     }
 
     void normal(Item item) {
-        if (item.quality > 0) {
-            item.quality--;
-        }
+        item.quality--;
 
-        if (item.quality > 0 && item.sellIn < 1) {
+        if (item.sellIn < 1) {
             item.quality--;
         }
 
